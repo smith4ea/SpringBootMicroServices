47055e7464cf795071973178c2339a0a5223d15d
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-01 14:10:47 -0400

    Update Controller

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,16 +1,35 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-
-
-
 public class PlantPlacesController {
 	
-	@RequestMapping("/start")
-	public String Start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=silver"})
+	public String readSilver() {
+		return "start";
+	}
+	
+	@PostMapping("/start")
+	public String Create() {
 		return "start";
 	}
 	
