fe449681e5e86fdf1444da23eca0b7b26f318724
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-08 15:01:52 -0400

    Autowired

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,15 +1,23 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
+
 @Controller
 public class PlantPlacesController {
 	
+	@Autowired
+	private ISpecimenService specimenServiceStub;
+	
 	@RequestMapping(value="/start", method=RequestMethod.GET)
 	public String read() {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
 		return "start";
 	}
 	
---
+++ src/main/java/com/plantplaces/service/ISpecimenService.java
@@ -0,0 +1,26 @@
+package com.plantplaces.service;
+
+import com.plantplaces.dto.SpecimenDTO;
+
+/**
+ * CRUD operations for specimens
+ * @author Administrator
+ *
+ */
+public interface ISpecimenService {
+
+	/**
+	 * Get specimens from persistence layer
+	 * @param id a unique lookup
+	 * @return a specimen with a matching id
+	 */
+	SpecimenDTO fetchById(int id);
+
+	/**
+	 * Persist the given DTO
+	 * @param specimenDTO
+	 *
+	 */
+	void Save(SpecimenDTO specimen);
+
+}
